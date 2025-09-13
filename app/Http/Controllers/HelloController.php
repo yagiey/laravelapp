e6a8c958f62bdaf6d1417f4aace83e27ce8ac00c
yagiey <yagiey@gmail.com>
2025-09-13 16:09:08 +0900

    p46「複数アクションの利用」

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,28 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HelloController extends Controller
-{
-    public function index($id='noname', $pass='unknown') {
-        return <<<EOF
-<html>
-<head>
-<title>Hello</title>
+global $head, $style, $body, $end;
+$head = '<html><head>';
+$style = <<<EOF
 <style>
 body { font-size: 16pt; color: #999; }
 h1 { font-size: 100pt; text-align: right; color: #eee;
     margin: -40px 0 -50px 0; }
 </style>
-</head>
-<body>
-    <h1>Index</h1>
-    <p>これは、Helloコントローラのindexアクションです。</p>
-    <ul>
-        <li>ID: {$id}</li>
-        <li>PASS: {$pass}</li>
-    </ul>
-</body>
-</html>
 EOF;
+$body = '</head><body>';
+$end = '</body></html>';
+
+function tag($tag, $text) {
+    return "<{$tag}>{$text}</{$tag}>";
+}
+
+class HelloController extends Controller
+{
+    public function index() {
+        global $head, $style, $body, $end;
+
+        $html =
+            $head .
+            tag('title', 'Hello/Index') .
+            $style .
+            $body .
+            tag('h1', 'Index') .
+            tag('p', 'Index page') .
+            '<a href="/hello/other">go to  Other page</a>' .
+            $end;
+        return $html;
+    }
+
+    public function other() {
+        global $head, $style, $body, $end;
+
+        $html =
+            $head .
+            tag('title', 'Hello/Other') .
+            $style .
+            $body .
+            tag('h1', 'Other') .
+            tag('p', 'Ondex page') .
+            $end;
+        return $html;
     }
 }
