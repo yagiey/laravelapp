df966ba60f6d37e796cef9404b375895d4e24979
yagiey <yagiey@gmail.com>
2025-09-15 17:40:18 +0900

    p64「ルートパラメータをテンプレートに渡す」

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,8 +6,11 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index() {
-        $data = ['msg' => 'これはコントローラから渡されたメッセージです。'];
+    public function index($id='zero') {
+        $data = [
+            'msg' => 'これはコントローラから渡されたメッセージです。',
+            'id' => $id
+        ];
         return view('hello.index', $data);
     }
 }
--- resources/views/hello/index.php
+++ resources/views/hello/index.php
@@ -14,5 +14,6 @@
     <h1>Index</h1>
     <p><?php echo $msg; ?></p>
     <p><?php echo date('Y年n月j日'); ?></p>
+    <p><?php echo $id ?></p>
 </body>
 </html>
