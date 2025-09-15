ebcede40c38819e9fc8de8b14039aa0e9684d591
yagiey <yagiey@gmail.com>
2025-09-15 17:33:02 +0900

    p62「コントローラでテンプレートを使う」

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,32 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response) {
-        $html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body { font-size: 16pt; color: #999; }
-h1 { font-size: 100pt; text-align: right; color: #eee;
-    margin: -40px 0 -50px 0; }
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <h3>Request</h3>
-    <pre>{$request}</pre>
-    <h3>Response</h3>
-    <pre>{$response}</pre>
-</body>
-</html>
-EOF;
-
-        $response->setContent($html);
-        return $response;
+    public function index() {
+        return view('hello.index');
     }
 }
