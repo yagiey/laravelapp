9a3fc182773c8153adf842d3fb57e013fd9debb5
yagiey <yagiey@gmail.com>
2025-09-15 17:37:29 +0900

    p62「値をテンプレートに渡す」

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     public function index() {
-        return view('hello.index');
+        $data = ['msg' => 'これはコントローラから渡されたメッセージです。'];
+        return view('hello.index', $data);
     }
 }
