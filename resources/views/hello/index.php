f52d0d07f0bec0ea61e805a7b140cf9855088422
yagiey <yagiey@gmail.com>
2025-09-15 17:30:28 +0900

    p60「ルートの設定でテンプレートを表示する」

---
+++ resources/views/hello/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello/Index</title>
+    <style>
+        body { font-size: 16pt; color: #999; }
+        h1 { font-size: 100pt; text-align: right; color: #eee;
+            margin: -40px 0 -50px 0; }
+    </style>
+</head>
+<body>
+    <h1>Index</h1>
+    <p>This is a sample page with php-template.</p>
+</body>
+</html>
