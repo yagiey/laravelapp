9ee9f99061ff99b07efbf0d5ef99130862fad350
yagiey <yagiey@gmail.com>
2025-09-16 16:50:33 +0900

    p83「@phpディレクティブについて」

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -12,15 +12,20 @@
 </head>
 <body>
     <h1>Blade/Index</h1>
-    <p>&#064;foreachディレクティブの例</p>
-    @foreach ($data as $item)
-    @if ($loop->first)
-    <p>※データ一覧</p><ul>
-    @endif
-    <li>No,{{$loop->iteration}}. {{$item}}</li>
-    @if ($loop->last)
-    </ul><p>--------ここまで</p>
-    @endif
-    @endforeach
+    <p>&#064;whileディレクティブの例</p>
+    <ol>
+    @php
+    $counter = 0;
+    @endphp
+
+    @while ($counter < count($data))
+    <li>{{$data[$counter]}}</li>
+
+    @php
+    $counter++;
+    @endphp
+
+    @endwhile
+    </ol>
 </body>
 </html>
