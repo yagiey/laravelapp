1b2ba3d151c2ba700fd34395f91edcad57870b1d
yagiey <yagiey@gmail.com>
2025-09-16 16:39:38 +0900

    p82「$loopによるループ変数」

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     public function index() {
-        return view('hello.index');
+        return view('hello.index', ['data' => ['one', 'two', 'three', 'four', 'five']]);
     }
 }
