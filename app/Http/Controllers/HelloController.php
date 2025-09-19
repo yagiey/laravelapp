a154cf86c88444233fd5add85c1d5f5a63092927
yagiey <yagiey@gmail.com>
2025-09-19 21:54:35 +0900

    p103「ビューコンポーザを利用する」

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,11 +7,6 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     public function index() {
-        $data = [
-            ['name' => '山田たろう', 'mail' => 'taro@yamada'],
-            ['name' => '田中はなこ', 'mail' => 'hanako@flower'],
-            ['name' => '鈴木さちこ', 'mail' => 'sachico@happy'],
-        ];
-        return view('hello.index', ['data' => $data]);
+        return view('hello.index', ['message' => 'Hello!']);
     }
 }
