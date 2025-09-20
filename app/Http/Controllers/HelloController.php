50893bd3f6798742c6a4f14bf759f4f304b19376
yagiey <yagiey@gmail.com>
2025-09-20 19:45:43 +0900

    p115「レスポンスを操作する」

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index(Request $request) {
-        return view('hello.index', ['data' => $request->data]);
+    public function index() {
+        return view('hello.index');
     }
 }
