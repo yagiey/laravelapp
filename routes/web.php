f68002a2c1eec6641361d7948e3e8de5ee4a6e85
yagiey <yagiey@gmail.com>
2025-09-22 04:29:43 +0000

    p118「グローバルミドルウェア」

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index')->middleware(HelloMiddleware::class);
+Route::get('hello', 'HelloController@index');
