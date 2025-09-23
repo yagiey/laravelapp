73a7c2df7b8c5b1e8dd152d096f54abbe9e12879
yagiey <yagiey@gmail.com>
2025-09-23 01:56:23 +0000

    p127「フィールドごとにエラーを表示」

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,20 +10,26 @@
 @section('content')
     <p>{{$msg}}</p>
     @if (count($errors) > 0)
-    <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{$error}}</li>
-            @endforeach
-        </ul>
-    </div>
+    <p>入力に問題がああります。再入力して下さい。</p>
     @endif
     <form action="/hello" method="post">
         <table>
             @csrf
+            @if ($errors->has('name'))
+            <tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>
+            @endif
             <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+
+            @if ($errors->has('mail'))
+            <tr><th>ERROR</th><td>{{$errors->first('mail')}}</td></tr>
+            @endif
             <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+
+            @if ($errors->has('age'))
+            <tr><th>ERROR</th><td>{{$errors->first('age')}}</td></tr>
+            @endif
             <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
+
             <tr><th></th><td><input type="submit" value="send"></td></tr>
         </table>
     </form>
