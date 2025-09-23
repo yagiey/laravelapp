725017017181dedd8f370adbc09d9fc3b6a82804
yagiey <yagiey@gmail.com>
2025-09-23 02:04:01 +0000

    p130「@errorディレクティブを使う」

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -15,19 +15,19 @@
     <form action="/hello" method="post">
         <table>
             @csrf
-            @if ($errors->has('name'))
-            <tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>
-            @endif
+            @error ('name')
+            <tr><th>ERROR</th><td>{{$message}}</td></tr>
+            @enderror
             <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
 
-            @if ($errors->has('mail'))
-            <tr><th>ERROR</th><td>{{$errors->first('mail')}}</td></tr>
-            @endif
+            @error ('mail')
+            <tr><th>ERROR</th><td>{{$message}}</td></tr>
+            @enderror
             <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
 
-            @if ($errors->has('age'))
-            <tr><th>ERROR</th><td>{{$errors->first('age')}}</td></tr>
-            @endif
+            @error ('age')
+            <tr><th>ERROR</th><td>{{$message}}</td></tr>
+            @enderror
             <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
 
             <tr><th></th><td><input type="submit" value="send"></td></tr>
