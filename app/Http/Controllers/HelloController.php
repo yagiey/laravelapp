3c778477fdf84ce759994bae01a1eb70272eb869
yagiey <yagiey@gmail.com>
2025-09-25 11:00:48 +0900

    p148「エラーメッセージのカスタマイズ」

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -21,11 +21,18 @@ class HelloController extends Controller
     }
 
     public function post(Request $request) {
-        $validator = Validator::make($request->all(), [
+        $rules = [
             'name' => 'required',
             'mail' => 'email',
             'age' => 'numeric|between:0,150',
-        ]);
+        ];
+        $messages = [
+            'name.required' => '名前は必ず入力して下さい。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢を整数で記入下さい。',
+            'age.between' => '年齢は0〜150の間で入力下さい。',
+        ];
+        $validator = Validator::make($request->all(), $rules, $messages);
         if ($validator->fails()) {
             return redirect('/hello')
                 ->withErrors($validator)
