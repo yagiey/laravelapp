dd89e5a0aa05cb5ff6cf6473c1cffc2c4d3e5013
yagiey <yagiey@gmail.com>
2025-12-15 16:47:06 +0900

    p199「updateによる更新」

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
 Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
+Route::get('hello/edit', 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
