c6efb9e23e2df4061d052cd506469bd9600d48e3
yagiey <yagiey@gmail.com>
2025-12-15 12:55:41 +0900

    p161「CSRF対策とVerifyCsrfToken」

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -14,7 +14,6 @@
     @endif
     <form action="/hello" method="post">
         <table>
-            @csrf
             @error ('name')
             <tr><th>ERROR</th><td>{{$message}}</td></tr>
             @enderror
