a0338727950947b1b4ed9f5d80eb41cb2aaada5f
yagiey <yagiey@gmail.com>
2025-12-15 13:36:39 +0900

    p163「クッキーを読み書きする」

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,25 +10,15 @@
 @section('content')
     <p>{{$msg}}</p>
     @if (count($errors) > 0)
-    <p>入力に問題がああります。再入力して下さい。</p>
+    <p>入力に問題があります。再入力して下さい。</p>
     @endif
     <form action="/hello" method="post">
         <table>
-            @error ('name')
-            <tr><th>ERROR</th><td>{{$message}}</td></tr>
-            @enderror
-            <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
-
-            @error ('mail')
-            <tr><th>ERROR</th><td>{{$message}}</td></tr>
-            @enderror
-            <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
-
-            @error ('age')
-            <tr><th>ERROR</th><td>{{$message}}</td></tr>
-            @enderror
-            <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
-
+            @csrf
+            @if ($errors->has('msg'))
+            <tr><th>ERROR</th><td>{{$errors->first('msg')}}</td></tr>
+            @endif
+            <tr><th>Message: </th><td><input type="text" name="msg" value="{{old('msg')}}"></td></tr>
             <tr><th></th><td><input type="submit" value="send"></td></tr>
         </table>
     </form>
