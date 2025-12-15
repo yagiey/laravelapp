b749d6282055a0c77382fc528782f217baca1097
yagiey <yagiey@gmail.com>
2025-12-15 15:55:49 +0900

    p192「DBクラスとは？」

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,27 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\HelloRequest;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        if ($request->hasCookie('msg')) {
-            $msg = 'Cookie: ' . $request->cookie('msg');
-        } else {
-            $msg = '※クッキーはありません。';
-        }
-        return view('hello.index', ['msg' => $msg]);
-    }
-
-    public function post(Request $request) {
-        $validate_rule = [
-            'msg' => 'required'
-        ];
-        $this->validate($request, $validate_rule);
-        $msg = $request->msg;
-        $response = response()->view('hello.index', ['msg' => '「' . $msg . '」をクッキーに保存しました。']);
-        $response->cookie('msg', $msg, 100);
-        return $response;
+        $items = DB::select('select * from people;');
+        return view('hello.index', ['items' => $items]);
     }
 }
