d8eeeedff186d6f35942edc269d03ab6c39310e1
yagiey <yagiey@gmail.com>
2025-12-16 10:43:10 +0900

    p207「指定したIDのレコードを得る」

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
+Route::get('hello/show', 'HelloController@show');
