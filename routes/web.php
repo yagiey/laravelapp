ee762ec4791ecfb8db2bce591278229dde2c66c5
yagiey <yagiey@gmail.com>
2025-12-16 09:52:09 +0900

    p202「deleteupdateによる削除」

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
 Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
